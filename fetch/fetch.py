be3dd3c7e8417a7ebabf42da85c731f447567310
Lav Shinde <13927503+dragoste17@users.noreply.github.com>
2021-12-05 13:47:43 +0900

    Write Basic Code to Download HTML

--- fetch/fetch.py
+++ fetch/fetch.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python
 
-print("Hello World")
+import urllib.request
+import urllib.error
+import urllib.parse
+
+url = 'https://www.google.com'
+
+response = urllib.request.urlopen(url)
+webContent = response.read().decode('UTF-8')
+
+filename = url.replace('https://', '')
+filename = filename.replace('http://', '')
+f = open(filename + '.html', 'w')
+f.write(webContent)
+f.close
