4fbf5ce5f77731a45ca17d7841ee52249af2774c
Lav Shinde <13927503+dragoste17@users.noreply.github.com>
2021-12-05 19:09:42 +0900

    Add Script to Support Metadata

--- fetch/fetch.py
+++ fetch/fetch.py
@@ -1,28 +1,72 @@
 #!/usr/bin/env python
 
 import argparse
+from bs4 import BeautifulSoup
+import os.path
+import time
 import urllib.request
-import urllib.error
-import urllib.parse
 
 
 def main():
-    urls = parseUrlsToList()
+    parser = setUpArgumentsParser()
+    args = parser.parse_args()
+
+    if (args.metadata):
+        generateMetadata(args.urls)
+    else:
+        fetchWebPages(args.urls)
+
+
+def setUpArgumentsParser():
+    parser = argparse.ArgumentParser(
+        description='Fetches web pages and saves them to disk')
+    parser.add_argument('urls', metavar='urls', nargs='+',
+                        help='A list of urls of web pages to download and save')
+    parser.add_argument('--metadata', action='store_true',
+                        help='Use this flag to see the metadata related to the urls')
+    return parser
+
+
+def generateMetadata(urls):
+    for url in urls:
+        try:
+            print()
+            generateMetadataForUrl(url)
+        except Exception as e:
+            print(url)
+            print(e)
+
+
+def generateMetadataForUrl(url):
+    site = generateSiteName(url)
+    print('site:', site)
+    filename = site + '.html'
+    if (os.path.exists(filename)):
+        printMetadata(filename)
+    else:
+        print('not yet fetched')
+
+
+def printMetadata(filename):
+    f = open(filename, 'r')
+    webContent = f.read()
+    f.close
+    soup = BeautifulSoup(webContent, 'html.parser')
+    links = soup.find_all('a')
+    print('num_links:', len(links))
+    images = soup.find_all('img')
+    print('images:', len(images))
+    fetchedTime = time.gmtime(os.path.getmtime(filename))
+    print(time.strftime("last_fetch: %a %b %d %Y %H:%M UTC", fetchedTime))
+
+
+def fetchWebPages(urls):
     for url in urls:
         try:
             fetchAndSaveWebPage(url)
         except Exception as e:
             print(url)
             print(e)
-
-
-def parseUrlsToList():
-    parser = argparse.ArgumentParser(
-        description="Fetches web pages and saves them to disk")
-    parser.add_argument('urls', metavar='urls', nargs='+',
-                        help='a list of urls of web pages to download and save')
-    args = parser.parse_args()
-    return args.urls
 
 
 def fetchAndSaveWebPage(url):
@@ -37,11 +81,16 @@ def fetchWebPage(url):
 
 
 def saveToFile(url, webContent):
-    filename = url.replace('https://', '')
-    filename = filename.replace('http://', '')
-    f = open(filename + '.html', 'w')
+    site = generateSiteName(url)
+    f = open(site + '.html', 'w')
     f.write(webContent)
     f.close
+
+
+def generateSiteName(url):
+    filename = url.replace('https://', '')
+    filename = filename.replace('http://', '')
+    return filename
 
 
 if __name__ == "__main__":
